c96fa148346ecde663c7e3790b20750e592d491f
Volodymyr_Kovalenko2 <voloymyr_kovalenko2@epam.com>
2020-06-11 23:15:09 +0300

    Added logging of login step

--- src/main/java/com/mentorship/action/login/UserLoginAction.java
+++ src/main/java/com/mentorship/action/login/UserLoginAction.java
@@ -3,12 +3,14 @@ package com.mentorship.action.login;
 import com.google.inject.Inject;
 import com.mentorship.model.UserDto;
 import com.mentorship.page.widget.HeaderWidget;
+import io.qameta.allure.Step;
 
 public class UserLoginAction {
 
     @Inject
     private HeaderWidget headerWidget;
 
+    @Step("Log in as user: {0}")
     public UserLoginAction logInAsUser(UserDto userDto) {
         headerWidget.clickOnLogInButton();
         headerWidget.typeEmail(userDto.getEmail());
